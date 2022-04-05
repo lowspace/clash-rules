2b692e4b5296eb7e229d96103fce27bbc9cececf
lowspace <lowspacelw@gmail.com>
2022-04-05 19:08:24 +0800

    add file

---
+++ dns_helper.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit 1; }
+{ command -v drill > /dev/null && dig=drill; } || { command -v dig > /dev/null && dig=dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
+
+
+
+NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
+
+PROVIDERS="
+223.5.5.5#223.5.5.5
+223.6.6.6#223.6.6.6
+119.29.29.29#119.29.29.29
+119.28.28.28#119.28.28.28
+114.114.114.114#114.114.114.114
+114.114.115.115#114.114.115.115
+"
+
+# Domains to test. Duplicated domains are ok
+DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com telegram.org whatsapp.com"
+
+
+totaldomains=0
+printf "%-18s" ""
+for d in $DOMAINS2TEST; do
+    totaldomains=$((totaldomains + 1))
+    # printf "%-8s" "test$totaldomains"
+done
+# printf "%-8s" "Average"
+echo ""
+
+
+for p in $NAMESERVERS $PROVIDERS; do
+    pip=${p%%#*}
+    pname=${p##*#}
+    ftime=0
+
+    printf "%-18s" "$pname"
+    for d in $DOMAINS2TEST; do
+        ttime=`$dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
+        if [ -z "$ttime" ]; then
+	        #let's have time out be 1s = 1000ms
+	        ttime=1000
+        elif [ "x$ttime" = "x0" ]; then
+	        ttime=1
+	    fi
+
+        # printf "%-8s" "$ttime ms"
+        ftime=$((ftime + ttime))
+    done
+    avg=`bc -lq <<< "scale=2; $ftime/$totaldomains"`
+
+    echo "  $avg"
+done
+
+
+exit 0;
